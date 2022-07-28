c71ee9090ff8261db43cbeb168f662a590883d45
William Jones <46946397+willjones11@users.noreply.github.com>
2022-07-28 00:26:23 -0400

    Update ric-heart.py
    Updated integration of python to work with html files in flask frame work.

--- ric-heart.py
+++ ric-heart.py
@@ -1,120 +1,98 @@
-// File: ./ric-heart.py
-#Algorithm start by outside call prob the start button 
-# have a way to where the user input starts the search of the results maybe from google
-#have a way where the inputs are stored and are ready to search when answer is wanted 
-#Types of Questions to ask:
-    #What types of Cuisine do you not like?   -input
-    #Any allegries?    input-free
-    #Dessert or dinner? "sweet" "savory".            #needs to be at front to determine the next set of questions
-    #What Spices do you want to use? input parsed
-    #Do you want vegtables? vegtables -vegtables
-    #Do you want fruit? fruit -fruit 
-    #Are you wanting a certian cusine? input
-    #Do you want soup? soup -soup
-    #Would you like a sandwich? sandwich -sandwich
-    
-
-#### TO DO and POSSIBLE THOUGHTS
-#
-#. maybe create the Input to where it outputs the user responses at the end to make changes or confirm before answer is submitted 
-#
-#  on name input check for profanities
-#  
-#
-#
-#importing packages to setup flask and app
-from flask import Flask, request, render_template
-from flask_socketio import SocketIO, emit 
+from flask import Flask, render_template, request
 from googlesearch import search
-app = Flask(__name__)
-socketio = SocketIO(app)
+import os
 
+
+app = Flask(__name__)
 
 #Storage of User Inputs might have to change later the input to a file needs to be where algo can repeatedly add onto to use inputs later to 
 #search recipes
-def _init_(self, name, Input):
-    self.name = None
-    self.Input = [] #client input 
-    self.Ques = [["Panckes or waffles?", "What fruit would you want?", "Would you want a cake or cupcakes?",  "What spices do you want to use?", "Do you want the dish cold or hot?"  ], [ "What meat do you want in your dish?", "Do you want a sandwich-like dish? (Sausage biscuit, bagel sandwich)", "What spices do you want to use?",
-     "Do you want vegetables?", "Do you want your dish cold or hot?"]] #[0]= sweet [1]=savory
-#starting the chatbox
-@app.route('/')
-def startup():
-    return render_template('ric-bot.html') #after pressing button to start chatbox it loads the html page of the chatbox
 
-#start of the interaction 
-@app.route('/introduce', methods=['GET', 'POST'])
-def start(self):
-    print("Hello My Name is RIC-BOT, lets find you a recipe today!") #greeting
-    self.name = input("What is your name? ") #intakes the clients name
-    #self.name  = request.args.get('username')
-    #self.Input = request.args.get('choice')
-    print("Hello,  ",  self.name ) #responds 
-    ans = input("Are we looking for Breakfast, Lunch, Dinner, or Dessert?") #interaction to start what type of meal client is looking for 
-    self.Input.append(ans)# adds answer to list of client inputs
-    interact() #calls interact function to begin conversation 
 
-@app.route('/', methods=['GET', 'POST'])
-def interact(self):
-    #checks for allergies 
-    print("Any food allegries or food to avoid")
-    x = input()
-    checker(x) #calls function to check answer
-    ans = input("Do you want sweet or savory meal?")
-    self.Input.append(ans)
-    if ans == "savory":
-        for i in range(0, len(self.Ques[1][1:])):
-            print(self.Ques[1][i])
-            an = input()
-            #if user wants to see their food answer from ric-bot
-            if an == "result":
-                break
-            elif an == "none":
-                continue
-            self.Input.extend(an)
+Input = [] #client input 
+Ques = ["Panckes or waffles?", "What fruit would you want?", "Would you want a cake or cupcakes?",  "What spices do you want to use?", "Do you want the dish cold or hot?", "Would you like soup or sandwhich" ]# [ "What meat do you want in your dish?", "Do you want a sandwich-like dish? (Sausage biscuit, bagel sandwich)", "What spices do you want to use?"
 
-        
-    if ans == "sweet":
-        for i in range(0, len(self.Ques[0][1:])):
-            print(self.Ques[0][i])
-            an = input()
-            #if user wants to see their food answer from ric-bot
-            if an == "result":
-                break
-            if an == "none":
-                continue
-            an = an.split()
-            self.Input.extend(an)
-    
-    end()
 
 
 #fixes wording for allergy food or unwanted ingredients
 #Setup for search
 #Allergy:   input-free
 #Unwanted:  -input
-def checker(self, x):
+def checker(x):
     #splits the client inputs
     ch = x.split()
+
     #Allergies
-    for i in len(ch):
-        if ch == "gluten" | ch == "shellfish" | ch == "peanut" | ch == "dairy":
-            self.Input.append(ch + "-free")
+    for i in ch:
+        if  i == "gluten" or i == "shellfish" or i == "peanut" or i == "dairy":
+            Input.append(i + "-free")
         #Uwanted Ingredients
         else:
-            self.Input.append("-" + ch)
+            Input.append("-" + i)
+    #picks up the next question in the bank     
+    nextQues = Ques[0]
+    Ques.pop(0) #getting rid of question so it is not reused again 
+    return nextQues
+    
 
 
+def interact(userText):
 
-@app.route('/end', methods=['POST'])    
-def end(self):
+    if len(Ques) == 0 or userText == "results":
+        botResponse = results()
+    elif userText == "":
+        #obtaining the next question in the bank and removing it since used 
+        botResponse = Ques[0]
+        Ques.pop(0)
+    elif userText == "yes":
+            userText.partition[-1] #gets the last word in the list which is the keyword?
+            keyword = userText.substring(0, userText.length() - 1)# getting rid of ? from keyword
+            Input.append(keyword)#adding to list
+    else:
+        #taking in user input and storing in list
+        x = userText.split(" ")
+        for word in x:
+            Input.append(word)
+        #obtaining the next question in the bank and removing it since used 
+        botResponse = Ques[0]
+        Ques.pop(0)
+
+    return botResponse
+        
+    
+#gives the user the result to the questions posed by RIC-BOT
+def results():
     #Convert list to String
-    query = ' '.join(self.Input)
+    query = ' '.join(Input) + ' recipe'
     #print out the google search result
-    print(search(query, tld="co.in", num=1, stop=1))
+    r = search(query, tld="co.in", num=1, stop=1)
+    return r
     
-    #Search for recipe 
-    print("Have a good day")
+
+#first page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+#the second page where the chat box is 
+@app.route("/chat")
+def home(): 
+	return render_template("home.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg') #intakes the use input 
+    #used for the inital start of the chatbox checking if their is no current inputs saved
+    if len(Input) == 0:
+        botResponse = checker(userText)
+    else:
+        botResponse = interact(userText)
+
+    return botResponse
     
-if __name__ == '__main__':
-    socketio.run(app)
+app.run(
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug=True
+)
